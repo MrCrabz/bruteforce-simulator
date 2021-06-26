06d4e88511bea77eb9c03299e9fbaade3e14747a
Robin Geagea <Robin.geagea.rg@gmail.com>
2021-06-27 01:28:36 +0300

    encryption feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, Component } from 'react';
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          plainText: ''
+      };
+      this.capturePlainText = this.capturePlainText.bind(this);
+      this.caesarCipher = this.caesarCipher.bind(this);
+    }
+
+
+  capturePlainText(event) {
+    this.setState({plainText: event.target.value});
+  }
+
+  caesarCipher() {
+    for (var cypherLevel = 1; cypherLevel < 26; cypherLevel++) {
+      // Captures the given plain text
+      let currentPlainText = this.state.plainText;
+      // Removes the spaces from the given plain text
+      currentPlainText = currentPlainText.replace(/\s/g, '');
+      // Decomposes the word into individual letters
+      let arrayofLetters = currentPlainText.split('');
+
+      // Cipher output
+      let cipherOutput = '';
+
+      // Magic Sauce
+      // Go through each character
+      for (var i = 0; i < currentPlainText.length; i++) {
+        // Get the character we'll be appending
+        var c = arrayofLetters[i];
+
+        // If it's a letter...
+        if (c.match(/[a-z]/i)) {
+          // Get its code
+          var code = currentPlainText.charCodeAt(i);
+
+          // Uppercase letters
+          if (code >= 65 && code <= 90) {
+            c = String.fromCharCode(((code - 65 + cypherLevel) % 26) + 65);
+          }
+
+          // Lowercase letters
+          else if (code >= 97 && code <= 122) {
+            c = String.fromCharCode(((code - 97 + cypherLevel) % 26) + 97);
+          }
+        }
+
+        // Append
+        cipherOutput += c;
+      }
+
+      console.log("K" + cypherLevel + ": " + cipherOutput);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="lateral lateralLeft col-lg-6 col-12">
+
+              <div className="encryptionInput">
+                <input
+                  type="text"
+                  id="plainText"
+                  placeholder="Type a phrase to..."
+                  value={this.state.plainText}
+                  onChange={this.capturePlainText}
+                />
+                <button
+                  id="executionButton"
+                  className="button"
+                  onClick={this.caesarCipher}
+                >
+                  Brute Force
+                </button>
+              </div>
+
+            </div>
+
+            <div className="lateral lateralRight col-lg-6 col-12">
+
+              <div className="outputBox">
+                {this.caesarCipher}
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  };
 }
 
 export default App;
